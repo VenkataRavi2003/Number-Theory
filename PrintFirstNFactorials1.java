7463a4ae57d116b0d885b26dd4e2c56d48dccafd
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-06 23:38:33 +0530

    Update PrintFirstNFactorials1.java

--- PrintFirstNFactorials1.java
+++ PrintFirstNFactorials1.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-class PrintFirstNFactorials1{
+class PrintFirstNFactorials{
 	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-		System.out.print("\nEnter 'N' > ");
-		int n=sc.nextInt();
-		long factorial=1;
-		for(int i=1;i<=n;++i){
-			factorial*=i;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter 'N' : ");
+		int n = sc.nextInt();
+		long factorial = 1;
+		for(int i = 1;i <= n; ++i){
+			
+			factorial *= i;
 			System.out.println(i+" != "+factorial);
 		}
+		sc.close();
 	}
 }
