36e47408b87da1125d1c26ce0f837937b4b6eaa8
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-27 17:25:48 +0530

    Add files via upload

---
+++ DecimalToBinary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class DecimalToBinary{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("\nEnter the number : ");
+		int num=sc.nextInt();
+		convertDecToBinary(num);
+	}
+	public static void convertDecToBinary(int num){
+		int binary[]=new int[30];
+		int index=0;
+		while(num>0){
+			binary[index++]=num%2;
+			num=num/2;
+		}
+		System.out.println();
+		for(int i=index-1;i>=0;--i){
+			System.out.print(binary[i]);
+		}
+	}
+}
