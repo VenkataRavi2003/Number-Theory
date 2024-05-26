cb24c1835f967406bded68a20f749a6ab239a2bb
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-26 19:09:12 +0530

    Add files via upload

---
+++ NumOfWordsInAString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class NumOfWordsInAString{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("\nEnter the sentence -> ");
+		String str=sc.nextLine();
+		int wordCount=1;
+		for(int i=0;i<str.length();++i){
+			if(str.charAt(i)==' '){
+				++wordCount;
+			}
+		}
+		System.out.println("\nNo.Of Words -> "+wordCount);
+	}
+}
