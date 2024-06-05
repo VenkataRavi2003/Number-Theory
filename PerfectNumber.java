0fc5b03957264d4b6d98454020b2e1e05af23bc6
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-06-05 16:32:44 +0530

    Add files via upload

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 public class PerfectNumber{
-	public static void main(String args[]){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("\nEnter the number : ");
 		int num = sc.nextInt();
-		
+
 		if(isPerfectNum(num)){
-			System.out.println("\n"+num+" -> Is Perfect Number.");
-		}else{
-			System.out.println("\n"+num+" -> Not Perfect Number!!");
+			System.out.println("\n"+num+" -> is perfect number.");
+		}else {
+			System.out.println("\n"+num+" -> not perfect number.");
 		}
+
 		sc.close();
 	}
 	public static boolean isPerfectNum(int num){
-		
 		int sum = 0;
-		
-		for(int i=1;i<num;++i){
-			if(num % i == 0)
+		for (int  i = 1; i < num ; ++i) {
+			
+			if(num % i == 0){
 				sum += i;
+			}
 		}
-		
+
 		return sum == num;
 	}
 }
